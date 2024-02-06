e3a84a3d37697ef96234cd324e47b5562d7e21fb
Akhil-ps-dev <psakhil211@gmail.com>
2024-02-06 22:20:29 +0530

    first

---
+++ config.js
@@ -0,0 +1,78 @@
+window.PorfolioConfig = {
+  name: 'Bryan Abraham',
+  jobTitle: 'Python Developer',
+  jobDescription: "I'm a Python Developer with experience in SQL, HTML, and JavaScript. I specialize in developing robust applications and have contributed to many projects, enhancing their functionality and user experience",
+  skills: [
+    {
+      name: 'Python',
+      description: ''
+    },  
+    {
+      name: 'MySQL ',
+      description: ''
+    }, 
+    {
+      name: 'JavaScript',
+      description: 'Reprehenderit mollit id ad veniam.'
+    },
+    {
+      name: 'HTML',
+      description: 'Fugiat veniam esse consequat quis cillum elit consequat Lorem pariatur ipsum veniam irure incididunt non.'
+    },
+    {
+      name: 'CSS',
+      description: 'Elit tempor mollit consectetur laborum nostrud qui in.'
+    }
+  ],
+  works: [
+    {
+      name: 'Vue Stripe Checkout',
+      link: 'https://jofftiquez.github.io/vue-stripe-checkout',
+      description: 'Mollit dolor tempor adipisicing non pariatur in tempor cillum do. In officia ullamco laborum ullamco voluptate. Laboris sunt quis enim Lorem laboris aliquip duis non magna excepteur voluptate. Pariatur velit aliquip officia nulla anim reprehenderit deserunt. Sint nostrud ipsum do aliquip.'
+    },
+    {
+      name: 'ToDo App',
+      link: 'https://theuselessweb.com/',
+      description: 'Esse magna anim incididunt enim sint aute consectetur ipsum id nostrud eu do. Pariatur et anim enim aliquip anim. Laboris labore incididunt labore enim nisi elit excepteur deserunt irure exercitation exercitation. Lorem sit ad laborum sit velit veniam sit nisi nostrud occaecat duis nulla duis.'
+    },
+    {
+      name: 'A Mobile Game',
+      link: 'https://theuselessweb.com/',
+      description: 'Incididunt non cupidatat deserunt ut commodo qui non do Lorem. Qui aute minim enim sit magna est consectetur incididunt. Ex est cupidatat tempor pariatur deserunt labore tempor minim laboris magna ipsum minim ea ea. Consectetur do anim laboris nulla nisi veniam sint qui.'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Learning a New Language',
+      // description: 'Occaecat ipsum dolor ea ad ut irure do proident excepteur consectetur duis eu culpa velit.'
+    },
+    {
+      name: 'Volunteering',
+      // description: 'Laboris occaecat adipisicing culpa duis aute ea excepteur quis eiusmod mollit est pariatur.'
+    },
+  
+    {
+      name: 'Cooking or Baking',
+      // description: 'Laborum pariatur sint labore officia enim occaecat id.'
+    }  
+  ],
+  contacts: {
+    email: 'abrahambryan2003@gmail.com',
+    mobileNo: '+91 9895772707',
+    // phoneNo: '+12 345 6789',
+    links: [
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/bryan-abraham-800002255/'
+      },
+      // {
+      //   name: 'Facebook',
+      //   link: 'https://facebook.com/jofftiquez'
+      // },
+      // {
+      //   name: 'Twitter',
+      //   link: 'https://twitter.com/jrtiquez'
+      // }
+    ]
+  }
+};
